e5ba9c5ab8c94dc4bd7f628f100263e5a2a277a0
Pmk2021 <pranavmk2003@gmail.com>
2024-11-24 18:20:54 -0800

    initial skeleton

---
+++ dyMES/model.py
@@ -0,0 +1,25 @@
+class dyMES:
+    def __init__(self) -> None:
+        pass
+
+    def update(self, time, dt, brute_force=True) -> None:
+        pass
+    
+    def brute_force_update() -> None:
+        pass
+
+    def lambda_dynamic_update() -> None:
+        pass
+
+    def find_steady_state_params() -> None:
+        pass
+
+    def R_mean() -> None:
+        #Calculate the sum R*f(n)
+        pass
+
+    def check_constraints(error_lim: float) -> bool:
+        pass
+
+    def reset() -> None:
+        pass
